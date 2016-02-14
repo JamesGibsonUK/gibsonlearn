33020a7a75b95a6fc1f37de46b2192c28be79288
James Gibson <jamesgibson@Jamess-Mac-Pro.local>
2016-02-14 19:12:36 +0000

    database config changes

--- craft/config/db.php
+++ craft/config/db.php
@@ -7,21 +7,20 @@
  * You can see a list of the default settings in craft/app/etc/config/defaults/db.php
  */
 
-return array(
-
-	// The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
-	'server' => 'localhost',
-
-	// The name of the database to select.
-	'database' => 'homestead',
-
-	// The database username to connect with.
-	'user' => 'homestead',
-
-	// The database password to connect with.
-	'password' => 'secret',
-
-	// The prefix to use when naming tables. This can be no more than 5 characters.
-	'tablePrefix' => 'learn',
-
-);
+ return array(
+     '*' => array(
+         'tablePrefix' => 'craft',
+     ),
+     '.dev' => array(
+         'server' => 'localhost',
+         'user' => 'homestead',
+         'password' => 'secret',
+         'database' => 'homestead',
+     ),
+     '.com' => array(
+         'server' => 'localhost',
+         'user' => 'forge',
+         'password' => 'KwKoA1ATuxPpKEfTohU7',
+         'database' => 'gibsonlearn',
+     ),
+ );
